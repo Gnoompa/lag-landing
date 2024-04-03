44a78e09dc461f5abe69e4d5f560948bf8c787cd
Gnoompa <gnoompa@Gnoompas-MacBook-Pro.local>
2024-04-03 14:56:55 +0400

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,206 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Button, Flex, Heading, Image, Link, Text } from "rebass";
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <Flex flexDirection={"column"} style={{ gap: "10rem" }}>
+      <div
+        style={{
+          position: "fixed",
+          width: "100vw",
+          height: "100vh",
+          backgroundImage: "url('/sand.svg')",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          zIndex: -1,
+        }}
+      />
+      <Image
+        display={["none", "block"]}
+        src="/lagui.png"
+        style={{
+          position: "absolute",
+          width: "25vw",
+          top: "10%",
+          left: "5%",
+        }}
+      ></Image>
+
+      <Image
+        display={["none", "block"]}
+        src="/lagui.png"
+        style={{
+          position: "absolute",
+          width: "25vw",
+          top: "10%",
+          right: "5%",
+          transform: "rotateY(180deg)",
+        }}
+      ></Image>
+      <Flex
+        mt={"10rem"}
+        width={"100%"}
+        flexDirection={"column"}
+        style={{ gap: "4rem" }}
+        alignItems={"center"}
+      >
+        <Flex flexDirection={"column"} style={{ gap: "2rem" }} alignItems={"center"}>
+          <Heading>
+            <Image src="/title.svg" width="30rem" maxWidth={"90vw"} />
+          </Heading>
+          <Text width={"29rem"} maxWidth={"90vw"} textAlign={"center"} fontSize={"1.25rem"}>
+            Tap, Earn, Dominate with LAG Clicker, on-chain game taking over 0xMantle in April 2024.
+          </Text>
+        </Flex>
+        <Button className="primary">PLAY</Button>
+      </Flex>
+      <Flex
+        flexDirection={"column"}
+        backgroundColor={"#FFFFE6"}
+        p={["1rem", "10rem"]}
+        style={{ gap: "2rem", border: "2px solid #111" }}
+      >
+        <Heading
+          as={"h2"}
+          color="#6EFEC2"
+          fontSize={"5rem"}
+          style={{
+            textShadow:
+              "2px 0 #111, -2px 0 #111, 0 2px #111, 0 -2px #111, 1px 1px #111, -1px -1px #111, 1px -1px #111, -1px 1px #111",
+          }}
+        >
+          About
+        </Heading>
+        <Flex style={{ gap: "10rem" }} flexDirection={["column", "row"]}>
+          <Text width={"50rem"} maxWidth={"90vw"} fontSize={"1.5rem"}>
+            LAG Clicker is a competitive on-chain game set to launch on the 0xMantle network in
+            April 2024. Players tap on worms or harvsters each round to earn $LAG.
+          </Text>
+          <Image src="/baron.png"></Image>
+        </Flex>
+      </Flex>
+      <Flex
+        flexDirection={"column"}
+        style={{ gap: "4rem", position: "relative" }}
+        alignItems={"center"}
+      >
+        <img
+          src="/spiced.png"
+          style={{
+            position: "absolute",
+            width: "40vw",
+            top: "3rem",
+          }}
+        ></img>
+        <Image src="/howto.svg" width={"20rem"}></Image>
+        <Flex style={{ gap: "2rem" }} mt={"10rem"} flexDirection={["column", "row"]}>
+          <Flex
+            flexDirection={"column"}
+            alignItems={"center"}
+            backgroundColor={"#FFFFE6"}
+            p={"2rem"}
+            className="bordered"
+            style={{ gap: "2rem" }}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+            <Image src="/lag.png" height={"15rem"}></Image>
+            <Text
+              color="#6EFEC2"
+              fontSize={"3rem"}
+              style={{
+                textShadow:
+                  "2px 0 #111, -2px 0 #111, 0 2px #111, 0 -2px #111, 1px 1px #111, -1px -1px #111, 1px -1px #111, -1px 1px #111",
+              }}
+            >
+              Slay opponents
+            </Text>
+          </Flex>
+          <Flex
+            flexDirection={"column"}
+            alignItems={"center"}
+            backgroundColor={"#FFFFE6"}
+            p={"2rem"}
+            className="bordered"
+            style={{ gap: "2rem" }}
+          >
+            <Image src="/mnt.svg" height={"15rem"}></Image>
+            <Text
+              color="#6EFEC2"
+              fontSize={"3rem"}
+              style={{
+                textShadow:
+                  "2px 0 #111, -2px 0 #111, 0 2px #111, 0 -2px #111, 1px 1px #111, -1px -1px #111, 1px -1px #111, -1px 1px #111",
+              }}
+            >
+              Stake $MNT
+            </Text>
+          </Flex>
+          <Flex
+            flexDirection={"column"}
+            alignItems={"center"}
+            backgroundColor={"#FFFFE6"}
+            p={"2rem"}
+            className="bordered"
+            style={{ gap: "2rem" }}
+          >
+            <Image src="/austin.png" height={"15rem"}></Image>
+            <Text
+              color="#6EFEC2"
+              fontSize={"3rem"}
+              style={{
+                textShadow:
+                  "2px 0 #111, -2px 0 #111, 0 2px #111, 0 -2px #111, 1px 1px #111, -1px -1px #111, 1px -1px #111, -1px 1px #111",
+              }}
+            >
+              Become OG
+            </Text>
+          </Flex>
+        </Flex>
+      </Flex>
+      <Flex
+        flexDirection={"column"}
+        backgroundColor={"#FFFFE6"}
+        p={["1rem", "10rem"]}
+        style={{ gap: "2rem", border: "2px solid #111" }}
+      >
+        <Heading
+          as={"h2"}
+          color="#6EFEC2"
+          fontSize={["3rem", "5rem"]}
+          style={{
+            textShadow:
+              "2px 0 #111, -2px 0 #111, 0 2px #111, 0 -2px #111, 1px 1px #111, -1px -1px #111, 1px -1px #111, -1px 1px #111",
+          }}
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          $LAGonomics
+        </Heading>
+        <Flex flexDirection={"column"}>
+          <Text fontWeight={"bold"}>Community Airdrops</Text>
+          <ul>
+            <li>20% Airdrop 1: Allocated to MNT stakers, genesis NFT holders and players.</li>
+            <li>30% Reserved for future airdrops</li>
+          </ul>
+          <Text fontWeight={"bold"}>Liquidity</Text>
+          <ul>
+            <li>15% for token liquidity </li>
+          </ul>
+          <Text fontWeight={"bold"}>Ecosystem</Text>
+          <ul>
+            <li>20% Ecosystem to expand and develop the LAG.</li>
+            <li>15% Team, investors and early contributors.</li>
+          </ul>
+        </Flex>
+        <hr color="#111" style={{ width: "100%" }} />
+        <Flex style={{ gap: "1rem" }} justifyContent={"flex-end"}>
+          <Link href="/" target="_blank">
+            <img src="/x.svg"></img>
+          </Link>
+          <Link href="/" target="_blank">
+            <img src="/discord.svg"></img>
+          </Link>
+          <Link href="/" target="_blank">
+            <img src="/warpcast.svg"></img>
+          </Link>
+        </Flex>
+      </Flex>
+    </Flex>
   );
 }
---
+++ theme.ts
@@ -0,0 +1,7 @@
+export default {
+    colors: {
+        primary: '#07c',
+        secondary: '#FFFFE6',
+        accent: "#6EFEC2"
+    },
+}
